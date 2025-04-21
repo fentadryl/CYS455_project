5137a8d29a7cf0b8954eea4740fb9f8d437e9e22
fentadryl <brolfe@my.ccsu.edu>
2025-04-21 19:40:48 -0400

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,52 @@
+//
+// Created by Ben on 4/21/2025.
+//
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+#include "movie.h"
+#include "file_io.h"
+#include "ui.h"
+#include "auth.h"
+
+int main() {
+    char filename[128];
+    Movie *movies = NULL;
+    int count = 0;
+
+    get_user_filename(filename, sizeof(filename));
+    load_movies(filename, &movies, &count); // <-- Load saved movies
+
+    int choice;
+    while (1) {
+        print_menu();
+        choice = safe_int_input();
+        if (choice == -1) continue;
+
+        switch (choice) {
+            case 1:
+                add_movie(&movies, &count);
+            break;
+            case 2:
+                list_movies(movies, count);
+            break;
+            case 3:
+                save_movies(filename, movies, count);
+            break;
+            case 4:
+                search_movies(movies, count);
+            break;
+            case 5:
+                filter_movies(movies, count);
+            break;
+            case 6:
+                free(movies);
+            printf("Exiting...\n");
+            return EXIT_SUCCESS;
+            default:
+                printf("Invalid option. Try again.\n");
+        }
+    }
+}
